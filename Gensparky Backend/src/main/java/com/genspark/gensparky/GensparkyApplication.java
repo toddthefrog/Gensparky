27d95991161a332fc35ed445fa5b00daf5a043cf
Todd Trowbridge <todd.m.trowbridge@gmail.com>
2022-03-01 09:03:36 -0500

    start.spring.io backend setup

---
+++ Gensparky Backend/src/main/java/com/genspark/gensparky/GensparkyApplication.java
@@ -0,0 +1,13 @@
+package com.genspark.gensparky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GensparkyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GensparkyApplication.class, args);
+	}
+
+}
---
+++ Gensparky Backend/src/test/java/com/genspark/gensparky/GensparkyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.gensparky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GensparkyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
