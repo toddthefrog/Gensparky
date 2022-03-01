6f306f2c8759d4bfa49d96d5fb740b5acbc469b0
Vi Nguyen <fluffywin@gmail.com>
2022-03-01 11:56:19 -0500

    setting up react

---
+++ Gensparky Frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Test from './components/Test';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <Test/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Gensparky Frontend/src/components/Test.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Test(){
+
+    return (
+        <div>
+            <h3>testing component</h3>
+        </div>
+    )
+}
