fe7ed484c78aae4d8438614cb104c80f1dd5fd68
Vi Nguyen <fluffywin@gmail.com>
2022-03-08 11:11:37 -0500

    fully-functional addUser component page

--- Gensparky Frontend/src/components/AddUserComponent.js
+++ Gensparky Frontend/src/components/AddUserComponent.js
@@ -7,6 +7,37 @@ class AddUserComponent extends React.Component{
 
     constructor(props){
         super(props);
+        this.state = {
+            active: '',
+            cohort: '',
+            discordName: '',
+            firstName: '',
+            lastName:''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    }
+
+    handleSubmit(){
+        let user = {
+            id: this.state.id,
+            firstName: this.state.firstName,
+            lastName: this.state.lastName,
+            active: this.state.active,
+            discordName: this.state.discordName,
+            cohort: this.state.cohort
+
+        }
+
+        UserService.addUser(user);
+        window.location.reload();
     }
 
     
@@ -26,10 +57,39 @@ class AddUserComponent extends React.Component{
 
     render(){
         return(
-            <button className="button--AddUser"
-                onClick={this.addUser}>
-                    testing addUser
-            </button>
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                <br/>
+                    <div>
+                        <label>First Name:</label>
+                        <input className="form--field" type="text" name="firstName" onChange={this.handleChange}/>
+                    </div>
+
+                    <div>
+                        <label>Last Name:</label>
+                        <input className="form--field" type="text" name="lastName" onChange={this.handleChange}/>
+                    </div>
+
+                    <div>
+                        <label>Active:</label>
+                        <input className="form--field" type="text" name="active" onChange={this.handleChange}/>
+                    </div>
+
+                    <div>
+                        <label>Discord Name:</label>
+                        <input className="form--field" type="text" name="discordName" onChange={this.handleChange}/>
+                    </div>
+
+                    <div>
+                        <label>Cohort:</label>
+                        <input className="form--field" type="text" name="cohort" onChange={this.handleChange}/>
+                    </div>
+
+                    <button className="btn--success" type="submit">Submit</button>
+
+                </form>
+                <br/>
+            </div>
         )
     }
 }
