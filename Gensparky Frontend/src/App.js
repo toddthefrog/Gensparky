abb275deec26f1197e4c25ea4abe495887c5c42e
AbbaAdam1 <sonicdx15@gmail.com>
2022-03-10 08:05:06 -0500

    Update App.js

--- Gensparky Frontend/src/App.js
+++ Gensparky Frontend/src/App.js
@@ -4,15 +4,31 @@ import UserComponent from './components/UserComponent';
 import AddUserComponent from './components/AddUserComponent';
 import DeleteUserComponent from './components/DeleteUserComponent';
 import UpdateUserComponent from './components/UpdateUserComponent';
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Link
+} from 'react-router-dom';
 
 function App() {
   return (
+  <Router>
     <div className="App">
-      <UserComponent />
-      <AddUserComponent/>
+             <li>
+               <Link to="/">User List</Link>
+             </li>
+             <li>
+               <Link to="/adduser">Register User</Link>
+             </li>
+                  <Routes>
+                  <Route exact path='/' element={< UserComponent />}></Route>
+                  <Route exact path='/adduser' element={< AddUserComponent />}></Route>
+                  </Routes>
       <br />
       <UpdateUserComponent/>
     </div>
+    </Router>
   );
 }
 
